d7c5028162003e3f44829c968ffc8985b939f15b
Eric Bidelman <ebidel@gmail.com>
2017-03-31 21:36:47 -0700

    adf

--- travis/runLighthouse.js
+++ travis/runLighthouse.js
@@ -91,7 +91,7 @@ function run(runner) {
 
 // Run LH if this is a PR.
 if (process.env.TRAVIS_EVENT_TYPE === 'pull_request') {
-  run(RUNNERS.wpt);
+  run(RUNNERS.chrome);
 } else {
   console.log('Lighthouse not run for non-PR commits');
 }
